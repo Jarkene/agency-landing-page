350f17583bc6868c2c6e6e13f3b66dac5ba6b06f
Aleksey Evlanov <jarkene@mail.ru>
2019-04-21 16:25:40 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.querySelector('.header-navbar__toggle-button').addEventListener('click', function() {
+    let navbar = document.querySelector('.header__navbar ul');
+    navbar.classList.toggle('show');
+})
